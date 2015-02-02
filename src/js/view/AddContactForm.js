41c4f30d3cbe2957d5c054356c59b53901ec443f
Alex Lemanski <alex.lemanski@bitfyre.net>
2015-02-02 07:44:09 -0800

    Add a basic validation method

--- src/js/view/AddContactForm.js
+++ src/js/view/AddContactForm.js
@@ -6,11 +6,29 @@ AddContactForm.prototype = {
   el: '.js-addContact',
 
   init: function() {
+    var addContact = this;
     var $form = document.querySelector(this.el);
-    $form.addEventListener('submit', this.onSubmit, false);
+    $form.addEventListener('submit', function() {
+      addContact.onSubmit();
+    }, false);
+  },
+
+  validate: function() {
+    var ids = [
+      'given-name',
+      'family-name',
+      'tel'
+    ];
+
+    ids.forEach(function(currentValue) {
+      if (document.getElementById(currentValue).value === '') {
+        console.error('empty input');
+      }
+    });
   },
 
   onSubmit: function() {
+    this.validate();
     console.log('Form Submit');
     event.preventDefault();
   }
