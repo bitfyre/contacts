e36ee4c52b6b28f8388a8660c69006df06e18904
Alex Lemanski <alex.lemanski@bitfyre.net>
2015-02-02 18:19:57 -0800

    Add controller/Contacts

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 var AddContactForm = require('./view/AddContactForm');
+var Contacts = require('./controller/Contacts');
 var Contact = require('./model/Contacts');
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var addContact = new AddContactForm();
+  var contacts = new Contacts();
 });
---
+++ src/js/controller/Contacts.js
@@ -0,0 +1,11 @@
+var ContactsController = function() {
+  this.init();
+};
+
+ContactsController.prototype = {
+  init: function() {
+    console.log('Contacts Controller');
+  }
+};
+
+module.exports = ContactsController;
