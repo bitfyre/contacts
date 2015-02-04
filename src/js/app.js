fa04512a776ae80d197e693112e886689192e6a2
Alex Lemanski <alex.lemanski@bitfyre.net>
2015-02-03 17:18:17 -0800

    Remove unnecessary dependencies in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 var ContactsController = require('./controller/Contacts');
-var Contact = require('./model/Contacts');
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var contacts = new ContactsController();
